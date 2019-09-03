1ff4d1c09daf9b015ea1e99045943d67bcb49538
Jung <thomas.jung@sap.com>
2019-09-03 14:37:36 -0400

    Add swagger/openAPI for OData V4 service endpoints

--- srv/routes/swagger.js
+++ srv/routes/swagger.js
@@ -1,5 +1,5 @@
 'use strict';
-module.exports = function(app) {
+module.exports = async (app) => {
 
 	const swaggerUi = require('swagger-ui-express')
 	let swaggerJSDoc = require('swagger-jsdoc')
@@ -9,12 +9,35 @@ module.exports = function(app) {
 			openapi: '3.0.0',
 			info: {
 				title: 'Bulletin Board Reviews',
-				version: '1.0.0'
+				version: '1.0.0',
+				"x-odata-version": '4.0'
 			}
 		},
 		apis: ['./routes/*']
 	}
 	var swaggerSpec = swaggerJSDoc(options)
+
+	const { parse, convert } = require('odata2openapi');
+	const cds = require("@sap/cds")
+	const csn = await cds.load(["../srv/gen/csn.json"])
+	let metadata = cds.compile.to.edmx(csn, { version: 'v4', })
+
+	const odataOptions = {}
+	try {
+		let service = await parse(metadata)
+		let swagger = await convert(service.entitySets, odataOptions, service.version)
+		console.log(JSON.stringify(swagger, null, 2))
+		Object.keys(swagger.paths).forEach(function(key) {
+			let val = swagger.paths[key]
+			swaggerSpec.paths[`/odata/v4/catalog${key}`] = val
+		})
+		swaggerSpec.definitions = swagger.definitions
+	} catch (error) {
+		console.error(error)
+	}
+
+
+
 	app.get('/api/api-docs.json', function (req, res) {
 		res.setHeader('Content-Type', 'application/json')
 		res.send(swaggerSpec)
